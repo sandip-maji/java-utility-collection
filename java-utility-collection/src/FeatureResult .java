c655b6cc27d765ee11f41ba521fea3581c89b859
Sandip Maji <sandip.jgec@gmail.com>
2025-04-16 16:37:37 +0530

    Create FeatureResult .java

---
+++ java-utility-collection/src/FeatureResult .java
@@ -0,0 +1,8 @@
+public class FeatureResult {
+    private String featureName;
+    private List<String> tags;
+    private String url;
+    private String status;
+    private List<ScenarioResult> scenarios;
+    // Getters & Setters
+}
