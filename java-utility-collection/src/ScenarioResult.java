b7a9dde414d67084a5c59072275420150d0f5c70
Sandip Maji <sandip.jgec@gmail.com>
2025-04-16 16:35:32 +0530

    Create ScenarioResult.java

---
+++ java-utility-collection/src/ScenarioResult.java
@@ -0,0 +1,7 @@
+public class ScenarioResult {
+    private String scenarioName;
+    private String status;
+    private List<String> tags;
+    private List<Steps> steps;
+    // Getters & Setters
+}
